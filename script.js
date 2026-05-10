51ea1d955f9e9d8448271238cd1f2e38b95e1953
coleclaycomb <cole23claycomb@gmail.com>
2026-05-10 12:27:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Get the mint radio button
-const mintFlavor = document.getElementById("mint");
-
-// Add event listener
-mintFlavor.addEventListener("click", function() {
-    alert("Out Of Stock");
-});
