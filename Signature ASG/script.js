aa88707907b312a10e5c1b2ea8f7e3f6e4fd5254
coleclaycomb <cole23claycomb@gmail.com>
2026-05-10 12:10:43 -0400

    Add files via upload

---
+++ Signature ASG/script.js
@@ -0,0 +1,7 @@
+// Get the mint radio button
+const mintFlavor = document.getElementById("mint");
+
+// Add event listener
+mintFlavor.addEventListener("click", function() {
+    alert("Out Of Stock");
+});
